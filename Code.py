3635f9fc429bd53b629fe5995871042706d9be4c
Pavel Grigorev <grigpasha05@gmail.com>
2023-12-05 12:06:12 +0300

    Сhanging the method of tracking the immobility of an object

--- Code.py
+++ Code.py
@@ -6,7 +6,7 @@ def detect_abandoned_item(video_file):
     video = cv2.VideoCapture(video_file)
     ret, frame = video.read()
 
-    coord = {}
+    dict_of_coord = {}
     color = (0, 0, 255)
     txt = 'ATTENTION! UNKNOWN OBJECT'
 
@@ -20,10 +20,10 @@ def detect_abandoned_item(video_file):
         ret, frame = video.read()
         gray_frame_next = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # преобразование в серый цвет
         frame_diff = cv2.absdiff(gray_frame, gray_frame_next)  # разница между кадрами
-        _, thresh = cv2.threshold(frame_diff, 30, 255, cv2.THRESH_BINARY)  # применение порога разницы
+        _, thresh_diff = cv2.threshold(frame_diff, 30, 255, cv2.THRESH_BINARY)  # применение порога разницы
 
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))  # удаление шумов
-        thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+        thresh = cv2.morphologyEx(thresh_diff, cv2.MORPH_OPEN, kernel)
 
         contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # нахождение контуров
 
@@ -33,12 +33,12 @@ def detect_abandoned_item(video_file):
 
             if area > 3000:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)  # отрисовка контуров
-                s = sum([x, y, x + w, y + h])
-                if s in coord:
-                    coord[s] += 1
+                coordinates = (x, y, x + w, y + h)
+                if coordinates in dict_of_coord:
+                    dict_of_coord[coordinates] += 1
                 else:
-                    coord[s] = 1
-                if coord[s] > 100:  # если сумма координат объекта остается неизменной на протяжении более 100 кадров
+                    dict_of_coord[coordinates] = 1
+                if dict_of_coord[coordinates] > 100:  # координаты объекта остаются неизменными на протяжении более 100 кадров
                     cv2.putText(frame, "abandoned item!", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, color,
                                 2)  # аларм на контуре
                     cv2.putText(frame, txt, (30, 30), cv2.FONT_HERSHEY_PLAIN, 2, color,
